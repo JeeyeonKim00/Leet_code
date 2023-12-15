83ebfa85cf12a72dc5d2576dc55c688efbd0ce5a
JeeyeonKim00 <127364024+JeeyeonKim00@users.noreply.github.com>
2023-12-15 09:56:33 +0900

    Time: 2057 ms (92.03%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,17 +1,14 @@
-# 영화에 최고점 준 사람 찾기.
 (SELECT U.name AS results
-FROM MovieRating MR
-LEFT JOIN Users U 
-ON MR.user_id = U.user_id
-GROUP BY U.name
-ORDER BY COUNT(MR.rating) DESC, U.name ASC
+FROM Users U
+LEFT JOIN MovieRating MR USING (user_id)
+GROUP BY U.user_id
+ORDER BY COUNT(MR.movie_id) DESC, U.name ASC
 LIMIT 1)
 UNION ALL
 (
 SELECT M.title 
-FROM MovieRating MR2
-LEFT JOIN Movies M
-ON MR2.movie_id = M.movie_id
+FROM Movies M
+LEFT JOIN MovieRating MR2 USING (movie_id)
 WHERE MR2.created_at LIKE '2020-02%'
 GROUP BY  M.title
 ORDER BY AVG(rating) DESC, M.title ASC
